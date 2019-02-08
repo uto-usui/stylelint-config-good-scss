2eb28a0b7a4c8a73dfb1f493b907a3b8809b4cb5
uto-usui <me@uto-usui.com>
2019-02-08 19:33:47 +0900

    ✏️typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict"
 
 module.exports = {
-  "extends": ["stylelint-config-recess-order", "stylelint-config-standerd"],
+  "extends": ["stylelint-config-recess-order", "stylelint-config-standard"],
   "plugins": ["stylelint-scss"],
   "rules": {
     "at-rule-no-unknown": null,
